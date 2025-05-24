20740fbc30e2b29b5b63a9e7c5e8c5edf6ff21e7
WybsonSantana <wybson@hotmail.com>
2025-05-24 19:07:23 -0300

    JSensors: #01 - 11.04 - Handling timeout errors

--- src/main/java/br/dev/s2w/jsensors/temperature/monitoring/api/controller/SensorMonitoringController.java
+++ src/main/java/br/dev/s2w/jsensors/temperature/monitoring/api/controller/SensorMonitoringController.java
@@ -6,9 +6,12 @@ import br.dev.s2w.jsensors.temperature.monitoring.domain.model.SensorMonitoring;
 import br.dev.s2w.jsensors.temperature.monitoring.domain.repository.SensorMonitoringRepository;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api/sensors/{sensorId}/monitoring")
@@ -45,8 +48,14 @@ public class SensorMonitoringController {
 
     @DeleteMapping("/enable")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @SneakyThrows
     public void disable(@PathVariable TSID sensorId) {
         SensorMonitoring sensorMonitoring = findByIdOrDefault(sensorId);
+
+        if (!sensorMonitoring.getEnabled()) {
+Thread.sleep(Duration.ofSeconds(10));
+        }
+
         sensorMonitoring.setEnabled(false);
 
         sensorMonitoringRepository.saveAndFlush(sensorMonitoring);
